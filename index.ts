23dc1d2e2e5472faf97f13de9b26c270a79aa5b7
tahasama <taha.maatof@gmail.com>
2022-07-15 13:44:29 +0100

    backend deploy

--- index.ts
+++ index.ts
@@ -26,6 +26,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/itn", itnRoute);
 app.use("/api/concrete", concreteRoute);
 app.use("/api/reinforcement", reinforcementRoute);
