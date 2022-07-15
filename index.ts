8f40a87d8eedecd514e2fb8a0c4ab0dc259ce4eb
tahasama <taha.maatof@gmail.com>
2022-07-15 14:11:20 +0100

    backend deploy

--- index.ts
+++ index.ts
@@ -23,15 +23,15 @@ mongoose
 const app = express();
 
 app.use(helmet());
-// app.use(cors());
+app.use(cors());
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 
